57846d8d222a7e9600cd95fce750e4c899a43fbb
jacki <jacki@JACKAGU>
2024-06-12 14:49:51 -0600

    Agregar archivos de proyecto.

---
+++ Analisis1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace Analisis1
+{
+    public partial class MainPage : ContentPage
+    {
+     
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+          
+
+            for (int i = 0; i <= 100; i += 2)
+            {
+               entry1.Text = entry1.Text + " " + i + "";
+            }
+
+           
+        }
+      
+            
+           
+        
+
+    }
+
+}
